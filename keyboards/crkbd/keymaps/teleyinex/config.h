479c2c0081697348feb1233313a863a8170d885c
Daniel Lombraña González <daniel.lombrana.g@astara.com>
2024-10-29 20:23:55 +0100

    fix: adapt tap term

--- keyboards/crkbd/keymaps/teleyinex/config.h
+++ keyboards/crkbd/keymaps/teleyinex/config.h
@@ -23,8 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define USE_MATRIX_I2C
 
 //#define QUICK_TAP_TERM 10
-#define TAPPING_TERM 200
-#define RETRO_TAPPING
+#define TAPPING_TERM 120
 
 #ifdef RGBLIGHT_ENABLE
     #define SPLIT_USB_DETECT
